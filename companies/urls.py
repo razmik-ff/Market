c0ae8696f4063458cd863d49a1f5e68d50ad0cd4
Razmik Poghosyan <rpoghosyan@fireflies.am>
2023-06-29 21:33:02 +0400

    Addedcompany model and simple pages

--- companies/urls.py
+++ companies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from companies import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<uuid:pk>', views.get_company, name='get_company'),
 ]
--- companies/views.py
+++ companies/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Company
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome, here")
+    result = Company.objects.all()
+    return render(request, 'companies.html', {'companies': result})
+
+def get_company(request, pk):
+    result = Company.objects.filter(pk=pk).first()
+    return render(request, 'company.html', {'company': result})
